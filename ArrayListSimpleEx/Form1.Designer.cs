8dff22d341a3eed599828b2e66770954c543b60f
Roman Pelikh <pelikh.roman@gmail.com>
2019-04-23 12:39:57 -0300

    init

--- ArrayListSimpleEx/Form1.Designer.cs
+++ ArrayListSimpleEx/Form1.Designer.cs
@@ -28,84 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnShowMsg = new System.Windows.Forms.Button();
-            this.btnReverse = new System.Windows.Forms.Button();
-            this.lblMessage = new System.Windows.Forms.Label();
-            this.txtSecondPos = new System.Windows.Forms.TextBox();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // btnShowMsg
-            // 
-            this.btnShowMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShowMsg.Location = new System.Drawing.Point(29, 47);
-            this.btnShowMsg.Name = "btnShowMsg";
-            this.btnShowMsg.Size = new System.Drawing.Size(188, 30);
-            this.btnShowMsg.TabIndex = 0;
-            this.btnShowMsg.Text = "Display Message";
-            this.btnShowMsg.UseVisualStyleBackColor = true;
-            // 
-            // btnReverse
-            // 
-            this.btnReverse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReverse.Location = new System.Drawing.Point(223, 47);
-            this.btnReverse.Name = "btnReverse";
-            this.btnReverse.Size = new System.Drawing.Size(188, 30);
-            this.btnReverse.TabIndex = 1;
-            this.btnReverse.Text = "Display Message In Reverse";
-            this.btnReverse.UseVisualStyleBackColor = true;
-            // 
-            // lblMessage
-            // 
-            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblMessage.Location = new System.Drawing.Point(26, 106);
-            this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(385, 23);
-            this.lblMessage.TabIndex = 2;
-            // 
-            // txtSecondPos
-            // 
-            this.txtSecondPos.Location = new System.Drawing.Point(29, 156);
-            this.txtSecondPos.Name = "txtSecondPos";
-            this.txtSecondPos.Size = new System.Drawing.Size(139, 20);
-            this.txtSecondPos.TabIndex = 3;
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(174, 154);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(193, 23);
-            this.btnAdd.TabIndex = 4;
-            this.btnAdd.Text = "Add to Second Position";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(29, 206);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(193, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Remove from Second Position";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 255);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.txtSecondPos);
-            this.Controls.Add(this.lblMessage);
-            this.Controls.Add(this.btnReverse);
-            this.Controls.Add(this.btnShowMsg);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.btnShowMsg = new System.Windows.Forms.Button();
+      this.btnReverse = new System.Windows.Forms.Button();
+      this.lblMessage = new System.Windows.Forms.Label();
+      this.txtSecondPos = new System.Windows.Forms.TextBox();
+      this.btnAdd = new System.Windows.Forms.Button();
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // btnShowMsg
+      // 
+      this.btnShowMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.btnShowMsg.Location = new System.Drawing.Point(29, 47);
+      this.btnShowMsg.Name = "btnShowMsg";
+      this.btnShowMsg.Size = new System.Drawing.Size(188, 30);
+      this.btnShowMsg.TabIndex = 0;
+      this.btnShowMsg.Text = "Display Message";
+      this.btnShowMsg.UseVisualStyleBackColor = true;
+      this.btnShowMsg.Click += new System.EventHandler(this.BtnShowMsg_Click);
+      // 
+      // btnReverse
+      // 
+      this.btnReverse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.btnReverse.Location = new System.Drawing.Point(223, 47);
+      this.btnReverse.Name = "btnReverse";
+      this.btnReverse.Size = new System.Drawing.Size(188, 30);
+      this.btnReverse.TabIndex = 1;
+      this.btnReverse.Text = "Display Message In Reverse";
+      this.btnReverse.UseVisualStyleBackColor = true;
+      // 
+      // lblMessage
+      // 
+      this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.lblMessage.Location = new System.Drawing.Point(26, 106);
+      this.lblMessage.Name = "lblMessage";
+      this.lblMessage.Size = new System.Drawing.Size(385, 23);
+      this.lblMessage.TabIndex = 2;
+      // 
+      // txtSecondPos
+      // 
+      this.txtSecondPos.Location = new System.Drawing.Point(29, 156);
+      this.txtSecondPos.Name = "txtSecondPos";
+      this.txtSecondPos.Size = new System.Drawing.Size(139, 20);
+      this.txtSecondPos.TabIndex = 3;
+      // 
+      // btnAdd
+      // 
+      this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.btnAdd.Location = new System.Drawing.Point(174, 154);
+      this.btnAdd.Name = "btnAdd";
+      this.btnAdd.Size = new System.Drawing.Size(193, 23);
+      this.btnAdd.TabIndex = 4;
+      this.btnAdd.Text = "Add to Second Position";
+      this.btnAdd.UseVisualStyleBackColor = true;
+      // 
+      // button1
+      // 
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.Location = new System.Drawing.Point(29, 206);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(193, 23);
+      this.button1.TabIndex = 5;
+      this.button1.Text = "Remove from Second Position";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(447, 255);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.btnAdd);
+      this.Controls.Add(this.txtSecondPos);
+      this.Controls.Add(this.lblMessage);
+      this.Controls.Add(this.btnReverse);
+      this.Controls.Add(this.btnShowMsg);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -7,14 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace ArrayListSimpleEx
 {
+
+   
     public partial class Form1 : Form
     {
+
+    ArrayList sentenceArr = new ArrayList();
+   
+
         public Form1()
         {
             InitializeComponent();
         }
+
+    private void BtnShowMsg_Click( object sender, EventArgs e )
+    {
+      string textToDisplay = "";
+
+      //foreach (string word in sentence )
+      //{
+      //  textToDisplay += $"{word} " ;
+      //}
+
+
+      lblMessage.Text = textToDisplay;
     }
+
+    private void Form1_Load( object sender, EventArgs e )
+    {
+
+      string sentence =  "I love programming so much";
+
+      string[] myStentence = sentence.Split(' ');
+ 
+      foreach(string word in myStentence)
+      {
+        sentenceArr.Add(word);
+      }
+   
+    }
+  }
 }
